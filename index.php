3b472ef5e7b03a9f208f494ad5ac2ecb4f9a48fa
James Tombs <james.tombs@utah.edu>
2018-10-26 08:52:49 -0600

    Initial Commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+    <title></title>
+
+    <!-- css -->
+    <link rel="stylesheet" href="css/main.css">
+    <!-- end css -->
+  </head>
+  <body>
+
+    <!-- page -->
+    <div class="page-container">
+
+      <!-- header -->
+      <header>
+        <div class="header-logo">
+          <a href="#">
+            <img src="img/universityofutah.svg" alt="University of Utah" class="img-fluid">
+          </a>
+        </div>
+        <div class="header-nav">
+          <nav>
+            <a href="#">About the U</a>
+            <a href="#">Admissions</a>
+            <a href="#">Academics</a>
+            <a href="#">The Arts</a>
+            <a href="#">Health Care</a>
+            <a href="#">Athletics</a>
+            <a href="#">Give</a>
+          </nav>
+        </div>
+        <div class="header-search">
+          <button id="uu-search-trigger" class="uu-search-btn" data-toggle="modal" data-target="#uu-search-modal">
+            <i class="fa fa-search" aria-hidden="true"></i>
+          </button>
+        </div>
+        <div class="header-menu">
+          <button id="uu-pushmenu-trigger" class="uu-hamburger uu-hamburger--x">
+            <span>MENU</span>
+          </button>
+        </div>
+      </header>
+      <!-- end header -->
+
+      <!-- main content -->
+      <main>
+
+        <!-- article -->
+        <article></article>
+        <!-- end article -->
+
+      </main>
+      <!-- end main content -->
+
+      <!-- footer -->
+      <footer></footer>
+      <!-- end footer -->
+
+    </div>
+    <!-- end page -->
+
+    <!-- scripts -->
+    <script src="js/custom.js"></script>
+    <!-- end scripts -->
+  </body>
+</html>
